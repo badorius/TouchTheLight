a9ddbe88e658ce482cca3355b8bfe9b5586e0286
badorius <salvadormuntane@gmail.com>
2023-11-22 22:40:08 +0100

    Add slide down jumpt and other sprites

--- godot/Scrpits/Arrow.gd
+++ godot/Scrpits/Arrow.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
 var speed = 750
+@export var direction : int = 1
 
 func _physics_process(delta):
-	position += transform.x * speed * delta
+
+	if direction == 1:
+		position += transform.x * speed * delta
+	if direction == -1:
+		position -= transform.x * speed * delta
 
 func _on_Arrow_body_entered(body):
 	if body.is_in_group("arrow"):
--- godot/Scrpits/Player.gd
+++ godot/Scrpits/Player.gd
@@ -9,68 +9,89 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var score : int = 0
 @onready var score_text : Label = get_node("../CanvasLayer/ScoreText")
 const Arrow = preload("../Objects/Arrow.tscn")
-
-
-
+var arrow_direction : int = 1
+#DEFAULT STATE START IDLE
+var state : String = "Idle"
 
 func _physics_process(delta):
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		state = "Jump"
+	else:
+		state = "Idle"
+		
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		state = "Jump"
+		
+	# Fire BOW
+	if Input.is_action_pressed("ui_fire1") and not Input.is_action_just_released("ui_fire1"):
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		state = "BowShooting"
+	if Input.is_action_just_released("ui_fire1"):
+			shoot(arrow_direction)
+			state = "Idle"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	
-	if Input.is_action_pressed("ui_fire1"):
-		$AnimationPlayer.play("BowShooting")
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if Input.is_action_just_pressed("ui_fire1"):
-			shoot()
-	else:
-		if direction:
-			if direction == -1:
-				get_node( "Warrior" ).set_flip_h( false )
-				velocity.x = direction * SPEED
-				$AnimationPlayer.play("WalkRight")
-			if direction == 1:
-				velocity.x = direction * SPEED
-				get_node( "Warrior" ).set_flip_h( true )
-				$AnimationPlayer.play("WalkRight")
-				
+	if direction:
+		if direction == -1:
+			get_node( "Warrior" ).set_flip_h( false )
+			arrow_direction = direction
+			velocity.x = direction * SPEED
+		if direction == 1:
+			velocity.x = direction * SPEED
+			get_node( "Warrior" ).set_flip_h( true )
+			arrow_direction = direction
+			
+		#SET STATE ON MOVEMENT
+		if not is_on_floor():
+			state = "Jump"
+		elif Input.is_action_pressed("ui_down"):
+			state = "SlideDown"
 		else:
-			$AnimationPlayer.stop()
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			state = "WalkRight"
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	#ANIMATION
+	$AnimationPlayer.play(state)
 	move_and_slide()
 	
+	#QUIT GAME
 	if Input.is_action_just_pressed("ui_cancel") and is_on_floor():
 		game_quit()
 	
+	#PLAYER DOWN GAMEOVER
 	if global_position.y > 250:
 		game_over()
 
+#GAMEOVER FUNCTION
 func game_over ():
 	get_tree().reload_current_scene()
-	
+
+#QUIT GAME FUNCTION
 func game_quit ():
 	get_tree().quit(0)
 	
+#ADD SCORE FUNCTION
 func add_score (amount):
 	score += amount
 	score_text.text = str("Score: ", score)
 	
-func shoot():
+#SHOOT ARROW FUNCTION
+func shoot(arrow_direction):
 	var main = get_tree().current_scene
 	var A = Arrow.instantiate()
 	A.global_position = global_position
 	A.position.x = global_position.x + 35
 	A.position.y = global_position.y + 25
+	A.direction = arrow_direction
 	
 	main.add_child(A)
 	
