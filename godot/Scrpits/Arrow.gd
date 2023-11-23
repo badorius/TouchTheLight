7caa90707ed1401c93d7119b40ea0425f399c2bc
badorius <salvadormuntane@gmail.com>
2023-11-23 23:01:04 +0100

    Add lights, fix row direction, bgmusic

--- godot/Scrpits/Arrow.gd
+++ godot/Scrpits/Arrow.gd
@@ -7,8 +7,10 @@ func _physics_process(delta):
 
 	if direction == 1:
 		position += transform.x * speed * delta
+		get_node( "Arrow" ).set_flip_h( false )
 	if direction == -1:
 		position -= transform.x * speed * delta
+		get_node( "Arrow" ).set_flip_h( true )
 
 func _on_Arrow_body_entered(body):
 	if body.is_in_group("arrow"):
