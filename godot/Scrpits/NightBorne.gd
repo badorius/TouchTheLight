2155a3cc540740a5c805275c5439d1394c61f384
badorius <salvadormuntane@gmail.com>
2023-12-09 13:06:22 +0100

    beer doesn't always help you

--- godot/Scrpits/NightBorne.gd
+++ godot/Scrpits/NightBorne.gd
@@ -24,26 +24,20 @@ func _process(delta):
 
 
 func _physics_process(delta):
-		# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		state = "Jump"
-	else:
-		state = "Idle"
-		
 	timer += delta
 	var player = get_parent().get_node("Player")
-	
-	#CHECK DISTANCE TO CHESE OR ATACK
+		#CHECK DISTANCE TO CHESE OR ATACK
 	if global_position.distance_to(player.global_position) < DISTANCE_THRESHOLD:
 		state = "ChasePlayer"
 		if global_position.distance_to(player.global_position) < DISTANCE_ATACK:
 			state = "Atack"
+	
+
 		
 	match state:
 		"idle":
 			if timer > 1.0:
-				var random_choice = randi() %3
+				var random_choice = randi() % 4
 				match random_choice:
 					0:
 						target_position = global_position + Vector2(randf_range(-50, -150), 0)
@@ -60,7 +54,7 @@ func _physics_process(delta):
 						$AnimatedSprite2D.play("idle")
 					3:
 						state = "jump"
-						$AnimatedSprite2D.play("idle")
+						$AnimatedSprite2D.play("run")
 						
 						
 
@@ -72,6 +66,9 @@ func _physics_process(delta):
 			if global_position.x < target_position.x or timer > 3.0:
 				state = "idle"
 				timer = 0
+				position = position
+				$AnimatedSprite2D.play("idle")
+
 
 		"WalkingRight":
 			velocity.x = SPEED
@@ -80,56 +77,62 @@ func _physics_process(delta):
 			if global_position.x > target_position.x or timer > 3.0:
 				state = "idle"
 				timer = 0
+				position = position
+				$AnimatedSprite2D.play("idle")
 				
 				
 		"jump":
-			if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			if is_on_floor():
 				velocity.y = JUMP_VELOCITY
 				state = "jump"
+			else:
+				state = "idle"
+				timer = 0
+				position = position
+				$AnimatedSprite2D.play("idle")
 
 		"ChasePlayer":
 			var direction = (player.global_position - global_position).normalized()
+			velocity= direction * SPEED
 			if direction.x > 0:
 				get_node( "AnimatedSprite2D" ).set_flip_h( false )
 				$AnimatedSprite2D.play("run")
 			else:
 				get_node( "AnimatedSprite2D" ).set_flip_h( true )
-				$AnimatedSprite2D.play("run")	
-			
-			velocity= direction * SPEED
-			$AnimatedSprite2D.play("run")
+				$AnimatedSprite2D.play("run")
 			if global_position.distance_to(player.global_position) < DISTANCE_THRESHOLD:
 				state = "idle"
+				timer = 0
+				position = position
+				$AnimatedSprite2D.play("idle")
 				
 		"Atack":
 			timer = 0
 			position = position
-			
 			var direction = (player.global_position - global_position).normalized()
 			if direction.x > 0:
-				position=position
 				get_node( "AnimatedSprite2D" ).set_flip_h( false )
 				$AnimatedSprite2D.play("atack")
 				player.hurt(1)
-
-
+				print ("atack")
 			else:
 				get_node( "AnimatedSprite2D" ).set_flip_h( true )
-				position=position
 				$AnimatedSprite2D.play("atack")
-				_on_animated_sprite_2d_animation_finished()
 				player.hurt(1)
-
-
+				print ("atack")
+				
 			if global_position.distance_to(player.global_position) < DISTANCE_THRESHOLD:
 				state = "idle"
+				timer = 0
+				position = position
+				$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
 
 
 func _on_body_entered(body):
 		if body.is_in_group("Player"):
-			body.hurt(10)
+			body.hurt(0.1)
 			
 func hurt(damage):
 		ArrowDamage_sound.play()
@@ -144,19 +147,26 @@ func explode():
 func death():
 	$AnimatedSprite2D.play("death")
 
-
-
 func _on_animated_sprite_2d_animation_finished():
 
 		await  $AnimatedSprite2D.animation_finished
 		print("Atack")
 		state = "idle"
+		timer = 0
+		position = position
+		$AnimatedSprite2D.play("idle")
 
 		await  $AnimatedSprite2D.animation_finished
 		print("hurt")
 		state = "idle"
+		timer = 0
+		position = position
+		$AnimatedSprite2D.play("idle")
 
 		await  $AnimatedSprite2D.animation_finished
 		print("death")
 		state = "idle"
+		timer = 0
+		position = position
+		$AnimatedSprite2D.play("idle")
 	
