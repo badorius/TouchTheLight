9df9ee2a15e8891dbcead354ab683f982d78bf7e
badorius <salvadormuntane@gmail.com>
2023-12-10 22:48:38 +0100

    No luck, worst movement

--- godot/Scrpits/NightBorne.gd
+++ godot/Scrpits/NightBorne.gd
@@ -20,121 +20,115 @@ func _ready():
 	ArrowDamage_sound = $ArrowDamage
 	#var player = get_parent().get_node("Player")
 
-	
 func _process(delta):
-	$ProgressBar.value = live
 	#CHECK DISTANCE TO CHESE OR ATACK
 	if global_position.distance_to(player.global_position) < DISTANCE_THRESHOLD:
 		state = "ChasePlayer"
 		if global_position.distance_to(player.global_position) < DISTANCE_ATACK:
 			state = "Attack"
-
 	
 func _physics_process(delta):
-
+	$ProgressBar.value = live
 	timer += delta
 	state_machine = $AnimationTree.get('parameters/playback')
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
+	
+			
 	match state:
 		"Iddle":
-			state_machine.travel('Start')
-			if timer > 1.0:
-				var random_choice = randi() % 4
-				match random_choice:
-					0:
-						target_position = global_position + Vector2(randf_range(-50, -150), 0)
-						state = "WalkingLeft"
-					1:
-						target_position = global_position + Vector2(randf_range(50, 150), 0)
-						state = "WalkingRight"
-					2:
-						state = "Iddle"
-						timer = 0
-					3:
-						state = "Jump"
-						
-						
+			set_iddle()
 		"WalkingLeft":
-			velocity.x = -SPEED
-			get_node( "Sprite2D" ).set_flip_h( true )
-			state_machine.travel('Run')
-			if global_position.x < target_position.x or timer > 3.0:
-				state = "Iddle"
-				timer = 0
-				state_machine.travel('Start')
-
-
-
+			set_walkingleft()
 		"WalkingRight":
-			velocity.x = SPEED
-			get_node( "Sprite2D" ).set_flip_h( false )
-			state_machine.travel('Run')
-			if global_position.x > target_position.x or timer > 3.0:
-				state = "Iddle"
-				timer = 0
-				state_machine.travel('Start')
-
-				
+			set_walkingright()
 		"Jump":
-			if is_on_floor():
-				velocity.y = JUMP_VELOCITY
-				state = "Jump"
-			else:
-				state = "Iddle"
-				timer = 0
-				state_machine.travel('Start')
-
-
+			set_jump()
 		"ChasePlayer":
-			var direction = (player.global_position - global_position).normalized()
-			if direction.x > 0:
-				get_node( "Sprite2D" ).set_flip_h( false )
-				velocity = direction * SPEED
-			else:
-				get_node( "Sprite2D" ).set_flip_h( true )
-				velocity = direction * SPEED
-
-			state_machine.travel('Run')	
-			if global_position.distance_to(player.global_position) > DISTANCE_THRESHOLD:
-				state = "Iddle"
-				timer = 0
-				state_machine.travel('Start')
-							
-				
-				
-				
+			set_chaseplayer()
 		"Attack":
-			timer = 0
-			var direction = (player.global_position - global_position).normalized()
-			if direction.x > 0:
-				get_node( "Sprite2D" ).set_flip_h( false )
-			else:
-				get_node( "Sprite2D" ).set_flip_h( true )
-			state_machine.travel('Attack')
-			if global_position.distance_to(player.global_position) > DISTANCE_THRESHOLD:
-				state = "Iddle"
-				timer = 0
-				state_machine.travel('Start')
-							
-							
-
-		
-
+			set_attack()
 
 	move_and_slide()
+	
+	
+func set_random_action():
+	if timer > 3.0:
+		var random_choice = randi() % 4
+		match random_choice:
+			0:
+				state = "WalkingLeft"
+			1:
+				state = "WalkingRight"
+			2:
+				state = "Iddle"
+			3:
+				state = "Jump"
+				
+func set_iddle():
+	state_machine.travel('Iddle')
+	state = "Iddle"
+	timer = 0
+	set_random_action()
 
+	
+func set_attack():
+	var direction = (player.global_position - global_position).normalized()
+	if direction.x > 0:
+		get_node( "Sprite2D" ).set_flip_h( false )
+	else:
+		get_node( "Sprite2D" ).set_flip_h( true )
+	state_machine.travel('Attack')
+	player.hurt(1)
+	if global_position.distance_to(player.global_position) > DISTANCE_THRESHOLD:
+		set_iddle()
+		set_random_action()
 
-
+		
+func set_chaseplayer():
+	var direction = (player.global_position - global_position).normalized()
+	if direction.x > 0:
+		get_node( "Sprite2D" ).set_flip_h( false )
+		state_machine.travel('Run')	
+		velocity = direction * SPEED
+	else:
+		get_node( "Sprite2D" ).set_flip_h( true )
+		state_machine.travel('Run')	
+		velocity = direction * SPEED
+	if global_position.distance_to(player.global_position) > DISTANCE_THRESHOLD:
+		set_iddle()
+		set_random_action()
+		
+func set_jump():
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		state = "Jump"
+	else:
+		set_iddle()
+		
+func set_walkingright():
+	target_position = global_position + Vector2(randf_range(50, 150), 0)
+	velocity.x = SPEED
+	get_node( "Sprite2D" ).set_flip_h( false )
+	state_machine.travel('Run')
+	if global_position.x > target_position.x or timer > 3.0:
+		set_iddle()
+		
+func set_walkingleft():
+	target_position = global_position + Vector2(randf_range(-50, -150), 0)
+	velocity.x = -SPEED
+	get_node( "Sprite2D" ).set_flip_h( true )
+	state_machine.travel('Run')
+	if global_position.x < target_position.x or timer > 3.0:
+		set_iddle()
 			
 func hurt(damage):
-		ArrowDamage_sound.play()
-		state_machine.travel('Hurt')
-		live -= damage
-		$ProgressBar.value = live
-		state = "ChasePlayer"
+	ArrowDamage_sound.play()
+	state_machine.travel('Hurt')
+	live -= damage
+	$ProgressBar.value = live
+	state = "ChasePlayer"
 		
 func explode():
 	pass
@@ -143,10 +137,3 @@ func death():
 	state_machine.travel('Death')
 	queue_free()
 
-
-
-
-
-	
-	
-			
