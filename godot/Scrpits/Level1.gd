5d7ecf3f243c8a192069b7f50bbba65ce74f6795
badorius <salvadormuntane@gmail.com>
2024-01-14 23:59:26 +0100

    Add explode enemy, add new mag1 enemy, change speed animations player and impact, add enemy sounds and impact

---
+++ godot/Scrpits/Level1.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var bgmusic : AudioStreamPlayer2D = get_node("AudioStreamPlayer2D") 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	#bgmusic.loop = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
