53de083681f361bc22914103b177b1e7deb3b203
badorius <salvadormuntane@gmail.com>
2024-01-22 13:34:18 +0100

    For your victory Ingred! one year

--- godot/Scrpits/DeathLamp.gd
+++ godot/Scrpits/DeathLamp.gd
@@ -4,16 +4,19 @@ var speed = 50.0
 var live = 30
 @onready var player : CharacterBody2D = get_node("../../Player")
 @onready var Explode : AnimationPlayer = get_node("EnemyExplode/AnimationPlayer")
+const DamageIndicator = preload("res://Objects/damage_indicator.tscn")
+@export var ArrowDamage_sound : AudioStreamPlayer2D
 var state : String = "Non"
 var state_machine
 @export var direction : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ArrowDamage_sound = $ArrowDamage
 	$DeathLampRise.visible = true
 	$DeathLampWalk.visible = false
-	$DeathLamp/CollisionShape2D.disabled = true
-	$CollisionShape2D2Walk.disabled = true
+	$DeathLamp/CollisionShape2D.disabled = false
+	$CollisionShape2D2Walk.disabled = false
 	$EnemyExplode.visible = false
 	
 	state_machine = $AnimationTree.get('parameters/playback')
@@ -49,3 +52,29 @@ func ready():
 func death():
 	state_machine.travel('Death')
 	Explode.play("Explode")
+	
+func hurt(damage):
+		if live <= 0 and state != "Death":
+			death()
+		
+		else:
+			ArrowDamage_sound.play()
+			live -= damage
+			#ProgressBar3.value = live
+			
+			#FIX Random size
+			var offset_position = randi() % 20
+			var main = get_tree().current_scene
+			var D = DamageIndicator.instantiate()
+			var color = "yellow"
+			D.global_position = Vector2(global_position.x - offset_position, (global_position.y) - offset_position)
+			D.show_damage(damage, color)
+			main.add_child(D)
+
+
+func _on_death_lamp_body_entered(body):
+	var attack_power = randi() % 10
+	if body.is_in_group("Player"):
+		body.hurt(attack_power)
+		live = 0
+ # Replace with function body.
