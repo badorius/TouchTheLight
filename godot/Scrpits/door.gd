3f907ffed9a55d6b74b3f2025effab611ed2a301
badorius <salvadormuntane@gmail.com>
2024-03-12 21:35:49 +0100

    add platforms up down lvl2

--- godot/Scrpits/door.gd
+++ godot/Scrpits/door.gd
@@ -15,4 +15,4 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://Objects/Levels/Level1Boss.tscn")
+		get_tree().change_scene_to_file("res://Objects/Levels/Level2.tscn")
---
+++ godot/Scrpits/platform_level_2v.gd
@@ -0,0 +1,23 @@
+extends AnimatableBody2D
+
+#Vars to Wave y movement
+var bob_height : float = 200.0
+var bob_speed : float = 1.0
+@onready var start_y : float = global_position.y
+var t : float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# wait 2 seconds
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+		# increase 't' over time.
+		t += delta
+		
+		# creloopate a sin wave that bobs up and down.
+		var d = (sin(t * bob_speed) + 1) / 2
+		
+		# apply that to  Y position.
+		global_position.y = start_y + (d * bob_height)
